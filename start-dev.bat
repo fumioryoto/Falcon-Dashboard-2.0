9f53ff4c0a0b48974126a547b9edc55660866ed6
Nahid <nahidhasanph79@gmail.com>
2026-02-07 03:30:43 +0600

    Update path to Falcon Dashboard in start-dev.bat

--- start-dev.bat
+++ start-dev.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Start dev server for Falcon Dashboard (adjust path if needed)
-cd /d "D:\codes"
+cd /d "D:\Falcon-Dashboard-2.0"
 REM open browser and start npm start in a minimized terminal
 start "" http://localhost:3000
 start /min cmd /c "npm start"
