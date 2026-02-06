664459a4953ca07466efd28cf9b83b5132fbd96d
Nahid <nahidhasanph79@gmail.com>
2026-02-07 03:31:05 +0600

    Update start-serve.bat

--- start-serve.bat
+++ start-serve.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Serve the production build for Falcon Dashboard.
 REM Install 'serve' globally first: npm i -g serve
-cd /d "D:\codes"
+cd /d "D:\Falcon-Dashboard-2.0"
 REM Run serve in a minimized terminal and open the browser
 start /min cmd /c "serve -s build --listen 3000"
 start "" http://localhost:3000
